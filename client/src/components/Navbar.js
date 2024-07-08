e20fe853768d8f6aac4ddb7b67b85268d5ca632a
jrohrbaugh0812 <jrohrbaugh3@ycp.edu>
2024-07-08 10:53:31 -0400

    Finished basic navbar and homepage.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,10 +4,10 @@ import '../css/components-css/Navbar.css';
 function Navbar() {
     return (
         <nav className="navbar">
-            <h1>Chat Swiftly</h1>
+            <h1>ChatSwiftly</h1>
             <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/login">Login</a></li>
+                <li><a href="/">Home</a></li> |
+                <li><a href="/login">Login</a></li> |
                 <li><a href="/register">Register</a></li>
             </ul>
         </nav>
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,14 +1,56 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import '../css/global.css';
 import '../css/pages-css/HomePage.css';
 
+const videos = [
+  '/videos/home_page_video_phone_1.mp4',
+  '/videos/home_page_video_phone_2.mp4',
+  '/videos/home_page_video_laptop_1.mp4',
+  '/videos/home_page_video_laptop_2.mp4'
+];
+
 function HomePage() {
-    return (
-        <div>
-            <h1>Welcome!</h1>
-            <p>Sign in <a href="http://localhost:3000">here</a>!</p>
-        </div>
-    )
+  const videoRef = useRef(null);
+  const [currentVideo, setCurrentVideo] = useState(0);
+
+  useEffect(() => {
+    const videoElement = videoRef.current;
+
+    const handleEnded = () => {
+        console.log('Video ended, currentVideo:', currentVideo);
+        setCurrentVideo((prevVideo) => (prevVideo + 1) % videos.length);
+    };
+      
+
+    videoElement.addEventListener('ended', handleEnded);
+
+    return () => {
+        videoElement.removeEventListener('ended', handleEnded);
+    }
+  }, [currentVideo]);
+
+  useEffect(() => {
+    const videoElement = videoRef.current;
+    videoElement.src = videos[currentVideo];
+    videoElement.play().catch((error) => {
+        console.error('Error playing the video: ', error);
+    });
+  }, [currentVideo]);
+
+  return (
+    <div className="homepage">
+      <div className={`video_container fade-animation`} key={currentVideo}>
+        <video ref={videoRef} autoPlay muted>
+          <source src={videos[0]} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      <div className="overlay">
+        <h1>Welcome!</h1>
+        <p>Sign in <a href="http://localhost:3000">here</a>!</p>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
