401d4c5c9eb6e1b7d69d7b8917e823da17f13c64
jrohrbaugh0812 <jrohrbaugh3@ycp.edu>
2024-07-09 16:41:48 -0400

    More comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
+// App.js: This file acts as the main entry point of the entire React application.
+
 import React from 'react';
 import AppRoutes from './routes';
 import Navbar from './components/Navbar';
 
+// This is the main function of the program. It renders the main structure of the application.
 function App() {
 
     return (
