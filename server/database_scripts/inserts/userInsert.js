d35947cdb5e5075b298afc7b6e42050888c243aa
jrohrbaugh0812 <jrohrbaugh3@ycp.edu>
2024-07-14 21:29:55 -0400

    Passwords are now salted and hashed before being inserted into the database.

--- server/database_scripts/inserts/userInsert.js
+++ server/database_scripts/inserts/userInsert.js
@@ -1,10 +1,13 @@
 // database_scripts/inserts/userInsert.js
 const { pool } = require('../../database_scripts/postgres-connection');
+const argon2 = require('argon2');
 
 async function insertNewUser(username, email, password) {
     try {
+        const hashedPassword = await argon2.hash(password);
+
         const client = await pool.connect();
-        await client.query('INSERT INTO Users (username, email, password_hash) VALUES ($1, $2, $3)', [username, email, password]);
+        await client.query('INSERT INTO Users (username, email, password_hash) VALUES ($1, $2, $3)', [username, email, hashedPassword]);
         client.release();
         return { success: true, message: 'Registration successful' };
     } catch (error) {
