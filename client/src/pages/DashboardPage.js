f40b535e2b5ec246a4a5673ffda86aee89fc0fda
jrohrbaugh0812 <jrohrbaugh3@ycp.edu>
2025-01-04 21:37:49 -0500

    Initial dashboard page layout.

--- client/src/pages/DashboardPage.js
+++ client/src/pages/DashboardPage.js
@@ -3,7 +3,14 @@ import '../css/global.css';
 import '../css/pages-css/DashboardPage.css';
 
 function DashboardPage() {
-    
+    return (
+        <div className="dashboard-page">
+            <div className="user-box">
+                <img src="/images/default-account-icon.png" alt="Profile" className="profile-picture" />
+                <h3 className="username">Username</h3>
+            </div>
+        </div>
+    )
 }
 
 export default DashboardPage;
