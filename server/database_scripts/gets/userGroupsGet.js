68aae884a51d0485076469d1ca50d248e895412d
jrohrbaugh0812 <jrohrbaugh3@ycp.edu>
2025-01-18 10:49:16 -0500

    Fix to return result instead of a Promise.

--- server/database_scripts/gets/userGroupsGet.js
+++ server/database_scripts/gets/userGroupsGet.js
@@ -1,21 +1,21 @@
 const { pool } = require('../postgres-connection');
 
 async function getUserGroupsByUserId(user_id) {
-    const client = await pool.connect();
+    const client = await pool.connect(); // Connect to the database
 
     try {
-        const result = client.query('SELECT * FROM user_groups WHERE user_id=$1', [user_id]);
+        const result = await client.query('SELECT * FROM user_groups WHERE user_id=$1', [user_id]); // Await the query
 
         if (result.rows.length > 0) {
-            return result.rows;
+            return result.rows; // Return the rows if found
         } else {
-            return null;
+            return null; // Return null if no rows found
         }
     } catch (error) {
         console.error('Error checking database:', error);
-        return { error: 'Database error' }
+        return { error: 'Database error' }; // Return an error object if an exception occurs
     } finally {
-        client.release();
+        client.release(); // Release the database client back to the pool
     }
 }
 
